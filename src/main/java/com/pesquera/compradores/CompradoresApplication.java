85fd03a0d66df7852073bfc6186470a369a95d71
eidan473 <eidanc1596.com>
2026-05-28 01:59:15 -0400

    feat(ms-compradores): entidades, repositorio, service, dto y controller

---
+++ src/main/java/com/pesquera/compradores/CompradoresApplication.java
@@ -0,0 +1,13 @@
+package com.pesquera.compradores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompradoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesquera/compradores/CompradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesquera.compradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
